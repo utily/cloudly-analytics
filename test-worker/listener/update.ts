a6d49b3a93d2ce16c024b7099f0e56373bee9204
AntonCroner <anton.kroner@pax2pay.com>
2022-12-16 17:34:35 +0100

    Fixed configuration and missing endpoint.

--- test-worker/listener/update.ts
+++ test-worker/listener/update.ts
@@ -1,4 +1,3 @@
-/*
 import * as cryptly from "cryptly"
 import * as gracely from "gracely"
 import * as analytics from "cloudly-analytics"
@@ -6,29 +5,29 @@ import * as http from "cloudly-http"
 import { Context } from "../Context"
 import { router } from "../router"
 
-export async function remove(request: http.Request, context: Context): Promise<http.Response.Like | any> {
+export async function update(request: http.Request, context: Context): Promise<http.Response.Like | any> {
 	let result: gracely.Result
-	const listener = await request.body
-	const owner = request.parameter.owner
+	let listener = await request.body
+	const id = request.parameter.id
 	const listeners = context.listeners
 	const key = await context.authenticate(request)
 	if (!key)
 		result = gracely.client.unauthorized()
-	else if (!owner || cryptly.Identifier.is(owner))
+	else if (!id || !cryptly.Identifier.is(id, 8))
 		result = gracely.client.invalidPathArgument(
-			"/listener/:owner",
-			"owner",
+			"/listener/:id",
+			"id",
 			"string",
-			"A valid owner identifier is required."
+			"A valid listener id identifier is required."
 		)
 	else if (!analytics.Listener.is(listener))
 		result = gracely.client.invalidContent("Listener", "Body is not a valid listener.")
 	else if (gracely.Error.is(listeners))
 		result = listeners
 	else {
+		listener = { ...listener, id: id }
 		result = gracely.success.created(listeners.listen(listener))
 	}
 	return result
 }
-router.add("DELETE", "/listener:owner", remove)
-*/
+router.add("PATCH", "/listener/:id", update)
