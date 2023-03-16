7dc07d1fe5c04d720c6e031cd63da40db6dc9d1d
Lasse Löfquist <github@tvartom.com>
2023-03-16 10:49:20 +0100

    Events with error and entityId.

--- packages/common/types/Event.ts
+++ packages/common/types/Event.ts
@@ -3,18 +3,20 @@ import { isly } from "isly"
 
 export interface Event {
 	source: string
-	entity: string
+	entity: { type: string; id?: string }
 	action: string
 	created?: isoly.DateTime
+	isError?: boolean
 }
 
 export namespace Event {
 	export const type = isly.object<Event>(
 		{
 			source: isly.string(),
-			entity: isly.string(),
+			entity: isly.object({ type: isly.string(), id: isly.string().optional() }),
 			action: isly.string(),
 			created: isly.fromIs("DateTime", isoly.DateTime.is).optional(),
+			isError: isly.boolean().optional(),
 		},
 		"Event"
 	)
