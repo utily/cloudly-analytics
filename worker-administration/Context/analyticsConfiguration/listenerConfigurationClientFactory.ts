40132be96c80ebe8e3e1c86347b975f5f55e55b1
Lasse Löfquist <tvartom@users.noreply.github.com>
2023-08-15 12:55:39 +0200

    Added documentation. (#21)

--- worker-administration/Context/analyticsConfiguration/listenerConfigurationClientFactory.ts
+++ worker-administration/Context/analyticsConfiguration/listenerConfigurationClientFactory.ts
@@ -5,6 +5,11 @@ import { Environment } from "Context/Environment"
 import { config as cloudlyAnalyticsDemoBigquery } from "./cloudly-analytics-demo-bigquery"
 import { config as cloudlyAnalyticsDemoLogger } from "./cloudly-analytics-demo-logger"
 
+/**
+ * Takes configuration in cloudly-analytics-demo-bigquery and cloudly-analytics-demo-logger.
+ *
+ * @returns
+ */
 export const listenerConfigurationClientFactory = (environment: Environment) => {
 	let result: gracely.Error | ListenerConfigurationClient
 	const privateKey = environment.bigQueryPrivateKey && JSON.parse(environment.bigQueryPrivateKey)
@@ -13,6 +18,8 @@ export const listenerConfigurationClientFactory = (environment: Environment) => 
 		result = gracely.server.misconfigured("bigQueryPrivateKey", "Private key for analytics is missing")
 	} else {
 		result = new ListenerConfigurationClient.TypescriptApi([
+			// Config is listed here.
+			// Inject privateKey for BigQuery from environment here:
 			Listener.Configuration.BigQuery.createConfiguration(cloudlyAnalyticsDemoBigquery, privateKey),
 			cloudlyAnalyticsDemoLogger,
 		])
--- worker-administration/README.md
+++ worker-administration/README.md
@@ -2,8 +2,27 @@
 
 Cloudflare Worker for storage and administration.
 
-## Setup
 
+## Setup (Current code with Typescript-API for listenerConfiguration)
+
+Configuration for listener is created in `./Context/listenerConfigurationClientFactory`.
+
+```
+nvm i
+```
+
+## Alternative Setup with listenerConfiguration in kv-value-space
+
+Delete:
+
+* `./storage`
+* `./Context/listenerConfigurationClientFactory`
+
+Remove `listenerConfigurationClientFactory` in `./Context/index.ts`.
+
+Let `BufferStorage` and `BucketStorage` be imported form `cloudly-analytics-storage` instead of `.storage`
+
+Create kv:namespace for listenerConfiguration:
 ```
 wrangler login
 wrangler kv:namespace create listenerConfiguration
