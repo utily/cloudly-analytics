c9bd42e5b953d0b7fddc2be99e6be33721624cc4
SiFalorSum <nilshansander@hotmail.com>
2024-07-31 11:29:52 +0200

    Improve event mapping to handle nested objects and arrays

--- packages/administration/Filter/Mapping/index.ts
+++ packages/administration/Filter/Mapping/index.ts
@@ -20,16 +20,13 @@ const transformers: Record<Transform, (value: any) => any> = {
 	array: value => Object.entries(value).map(([key, value]) => ({ key, value })), //To handle records with generic keys
 }
 
-function transform(type: Getter["transform"], value: any | undefined): any {
-	return type && value != undefined ? transformers[type](value) : value
-}
 type RecordWithSelector<T extends string> = Record<string, T | Getter<T>>
 
 type MaybeArray<T> = T | T[]
 type Getter<T extends string = string> = {
 	selector: MaybeArray<T>
 	default?: MaybeArray<number | string | boolean>
-	transform?: Transform
+	transform?: Transform | RecordWithSelector<string>
 }
 
 export interface Mapping extends BaseFilter.Configuration {
@@ -66,27 +63,53 @@ export namespace Mapping {
 	export const flaw = type.flaw
 
 	export class Implementation extends BaseFilter<Mapping> {
-		filter(event: types.EventWithMetadata | object): types.EventWithMetadata | object | undefined {
-			return Object.entries(this.filterConfiguration.mapping).reduce((object, [setSelector, getterValue]) => {
-				const getter: Getter = typeof getterValue == "string" ? { selector: getterValue } : getterValue
-
-				return Selector.set(
-					object,
-					setSelector,
-					transform(
-						getter.transform,
-						!getter.selector
-							? getter.default
-							: Array.isArray(getter.selector)
-							? getter.selector.map(
-									(getSelector, index) =>
-										Selector.get(event, getSelector) ??
-										(Array.isArray(getter.default) ? getter.default[index] : getter.default)
-							  )
-							: Selector.get(event, getter.selector) ?? getter.default
-					)
+		filter(
+			event: types.EventWithMetadata | object,
+			mapping?: RecordWithSelector<types.EventWithMetadata.Selector | (Selector & Record<never, never>)>
+		): types.EventWithMetadata | object | undefined {
+			return Object.entries(mapping ?? this.filterConfiguration.mapping).reduce(
+				(object, [setSelector, getterValue]) => {
+					const getter: Getter = typeof getterValue == "string" ? { selector: getterValue } : getterValue
+					return this.set(object, setSelector, event, getter)
+				},
+				{}
+			)
+		}
+		private set<T extends any[] | Record<string, any>>(
+			data: T,
+			selector: Selector,
+			event: types.EventWithMetadata | object,
+			getter: Getter
+		): T {
+			return Selector.set(
+				data,
+				selector,
+				this.transform(
+					getter.transform,
+					!getter.selector
+						? getter.default
+						: Array.isArray(getter.selector)
+						? getter.selector.map(
+								(getSelector, index) =>
+									Selector.get(event, getSelector) ??
+									(Array.isArray(getter.default) ? getter.default[index] : getter.default)
+						  )
+						: Selector.get(event, getter.selector) ?? getter.default,
+					Array.isArray(getter.selector) ? getter.selector.some(s => s == "*") : getter.selector.includes("*")
 				)
-			}, {})
+			)
+		}
+		private transform(type: Getter["transform"], value: any | undefined, map: boolean): any {
+			console.log("transform map:", map)
+			return !type || value == undefined
+				? value
+				: typeof type == "string"
+				? map && Array.isArray(value) // TODO: make it work for nested arrays
+					? value.map(item => transformers[type](item))
+					: transformers[type](value)
+				: Array.isArray(value)
+				? value.map(item => this.filter(item, type))
+				: this.filter(value, type)
 		}
 	}
 }
---
+++ packages/administration/Filter/Mapping/mapping.spec.ts
@@ -0,0 +1,63 @@
+import { Mapping } from "./index"
+
+describe("Mapping and filtering", () => {
+	const event = {
+		stringVal: "test",
+		numVal: 2,
+		tuple: ["key", { inner: "value" }],
+		array: [{ inner: "value0" }, { inner: "value1" }, { inner: "value2" }, { inner: "value3" }],
+		nested: {
+			property: "value",
+			array: [{ nested: { property: "value" } }],
+			doubleArray: [
+				[{ inner: 1 }, { inner: 2 }],
+				[{ inner: 3 }, { inner: 4 }],
+			],
+		},
+		map: { prop1: "hej", prop2: "haj" },
+	}
+	const mapping: Mapping = {
+		type: "mapping",
+		mapping: {
+			stringVal: "stringVal",
+			numVal: { selector: "numVal", transform: "integer" },
+			tupleKey: "tuple[0]",
+			tupleInner: "tuple[1].inner",
+			arrayInner: "array[*].inner",
+			arrayFlatten: { selector: "array", transform: "stringify" },
+			arrayFlattenItems: { selector: "array[*]", transform: "stringify" },
+			nestedProp: "nested.property",
+			nestedArray: {
+				selector: "nested.array",
+				transform: {
+					arrayNestedProp: "nested.property",
+					arrayNestedFlatten: { selector: "nested", transform: "stringify" },
+				},
+			},
+			nestedDoubleArray: "nested.doubleArray[*][*].inner",
+			map: { selector: "map", transform: "array" },
+		},
+	}
+	const result = {
+		stringVal: "test",
+		numVal: 2,
+		tupleKey: "key",
+		tupleInner: "value",
+		arrayInner: ["value0", "value1", "value2", "value3"],
+		arrayFlatten: '[{"inner":"value0"},{"inner":"value1"},{"inner":"value2"},{"inner":"value3"}]',
+		arrayFlattenItems: ['{"inner":"value0"}', '{"inner":"value1"}', '{"inner":"value2"}', '{"inner":"value3"}'],
+		nestedProp: "value",
+		nestedArray: [{ arrayNestedProp: "value", arrayNestedFlatten: '{"property":"value"}' }],
+		nestedDoubleArray: [
+			[1, 2],
+			[3, 4],
+		],
+		map: [
+			{ key: "prop1", value: "hej" },
+			{ key: "prop2", value: "haj" },
+		],
+	}
+	it("Maps event", () => {
+		expect(new Mapping.Implementation(mapping).filter(event)).toEqual(result)
+	})
+})
